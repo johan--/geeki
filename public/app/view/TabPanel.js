7c870ea72ac8ee0a2160d970f80182125d2dda83
Łukasz Bandzarewicz <lucassus@gmail.com>
2012-01-11 17:35:52 +0100

    Tune panels.

--- public/app/view/TabPanel.js
+++ public/app/view/TabPanel.js
@@ -1,6 +1,8 @@
 Ext.define('Rwiki.view.TabPanel', {
   extend: 'Ext.tab.Panel',
   alias: 'widget.rwiki.tabPanel',
+
+  title: 'Rwiki TabPanel',
   items: [{
     title: 'Tab one'
   }, {
--- public/app/view/TreePanel.js
+++ public/app/view/TreePanel.js
@@ -1,8 +1,12 @@
 Ext.define('Rwiki.view.TreePanel', {
-  requires: ['Rwiki.store.TreeStore'],
-
   extend: 'Ext.tree.Panel',
   alias: 'widget.rwiki.treePanel',
+  requires: ['Rwiki.store.TreeStore'],
+
+  title: 'Rwiki Pages',
+  width: 300,
+  collapsible: true,
+  resizable: true,
   store: Ext.create('Rwiki.store.TreeStore')
 });
 
